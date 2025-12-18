a0af1523cc6c8f1911695ed7d1e87529c37552d3
bala-amypo <bala@amypo.in>
2025-12-18 10:07:25 +0000

    User commit

--- src/main/java/com/example/demo/dto/ApiResponse.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.dto;
-
-public class ApiResponse {
-    public String message;
-
-    public ApiResponse(String message) {
-        this.message = message;
-    }
-}
--- src/main/java/com/example/demo/repository/TierHistoryRecordRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.TierHistoryRecord;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TierHistoryRecordRepository extends JpaRepository<TierHistoryRecord, Long> {
-}
