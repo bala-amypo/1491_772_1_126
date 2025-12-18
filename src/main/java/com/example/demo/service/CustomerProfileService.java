0c2366387e691f8c796a790747f7f507755162e7
bala-amypo <bala@amypo.in>
2025-12-18 06:29:38 +0000

    User commit

--- src/main/java/com/example/demo/service/CustomerProfileService.java
+++ src/main/java/com/example/demo/service/CustomerProfileService.java
@@ -1,24 +1,11 @@
 package com.example.demo.service;
 
 import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.example.demo.entity.CustomerProfile;
-import com.example.demo.repository.CustomerProfileRepository;
 
-@Service
-public class CustomerProfileService {
+public interface CustomerProfileService {
 
-    @Autowired
-    CustomerProfileRepository repository;
+    CustomerProfile createCustomer(CustomerProfile cp);
 
-    public CustomerProfile createCustomer(CustomerProfile cp) {
-        return repository.save(cp);
-    }
-
-    public List<CustomerProfile> getAllCustomers() {
-        return repository.findAll();
-    }
+    List<CustomerProfile> getAllCustomers();
 }
