1cd8a8da1c47cb7fa078790ba20d4f3e25bd641f
bala-amypo <bala@amypo.in>
2025-12-18 09:02:25 +0000

    User commit

--- src/main/java/com/example/demo/entity/AuthRequest.java
+++ src/main/java/com/example/demo/entity/AuthRequest.java
@@ -1,12 +1,30 @@
 package com.example.demo.entity;
 
 public class AuthRequest {
+
     private String username;
     private String password;
 
-    // Getters and Setters
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
+    public AuthRequest() {}
+
+    public AuthRequest(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
