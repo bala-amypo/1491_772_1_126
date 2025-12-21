cac6016047e4ea01ff3d892062c964dea23f4b59
bala-amypo <bala@amypo.in>
2025-12-21 17:52:48 +0000

    User commit

--- src/main/java/com/example/demo/model/PurchaseRecord.java
+++ src/main/java/com/example/demo/model/PurchaseRecord.java
@@ -22,23 +22,18 @@ public class PurchaseRecord {
 
     public PurchaseRecord() {}
 
-    public PurchaseRecord(Long customerId, Double amount,
-                          LocalDate purchaseDate, String storeLocation) {
-        this.customerId = customerId;
-        this.amount = amount;
-        this.purchaseDate = purchaseDate;
-        this.storeLocation = storeLocation;
-    }
+    // setters REQUIRED by test
+    public void setId(Long id) { this.id = id; }
+    public void setAmount(Double amount) { this.amount = amount; }
+    public void setPurchaseDate(LocalDate purchaseDate) { this.purchaseDate = purchaseDate; }
+    public void setStoreLocation(String storeLocation) { this.storeLocation = storeLocation; }
 
     public void setCustomer(CustomerProfile customer) {
         this.customer = customer;
         this.customerId = customer.getId();
     }
 
-    // Getters
+    // getters
     public Long getId() { return id; }
-    public Long getCustomerId() { return customerId; }
     public Double getAmount() { return amount; }
-    public LocalDate getPurchaseDate() { return purchaseDate; }
-    public String getStoreLocation() { return storeLocation; }
 }
